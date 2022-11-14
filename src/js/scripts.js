6fef5d8806346e9eb2c0aa20936c45c4742e1abb
LeonelFreitas <92731087+LeonelFreitas@users.noreply.github.com>
2022-11-14 10:13:50 -0300

    Add files via upload

---
+++ src/js/scripts.js
@@ -0,0 +1,22 @@
+function alerta()
+    {
+    alert("Qualquer dúvida pode ser sanada através de nosso chatbot 🤖");
+    }
+	
+  
+    (function(s, z, c, h, a, t) {
+      s.webchat = s.webchat || function() {
+        (s.webchat.q = s.webchat.q || []).push(arguments);
+      };
+      t = z.createElement(c),
+        a = z.getElementsByTagName(c)[0]; t.async = 1;
+      t.src = 'https://foa.sz.chat/webchat/v2/webchat.js'; 
+      a.parentNode.insertBefore(t, a);
+      s.webchat('cid', '5fdbd59f1350dd1c60113e3b');
+      s.webchat('host', h);
+    })(window, document, 'script', 'https://foa.sz.chat');
+
+    window.addEventListener("message", function(event) {
+      if(event.origin !== 'https://foa.sz.chat') return;
+    },false);
+
